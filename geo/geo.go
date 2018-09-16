f6b34d62f4288ede6aec88034ae8f5e5cac6d1d2
tidwall <joshbaker77@gmail.com>
2018-09-16 14:03:33 -0700

    Added BearingTo and tests

--- geo/geo.go
+++ geo/geo.go
@@ -2,9 +2,11 @@ package geo
 
 import "math"
 
-const earthRadius = 6371e3
-const radians = math.Pi / 180
-const degrees = 180 / math.Pi
+const (
+	earthRadius = 6371e3
+	radians     = math.Pi / 180
+	degrees     = 180 / math.Pi
+)
 
 // DistanceTo return the distance in meteres between two point.
 func DistanceTo(latA, lonA, latB, lonB float64) (meters float64) {
@@ -14,20 +16,41 @@ func DistanceTo(latA, lonA, latB, lonB float64) (meters float64) {
 	λ2 := lonB * radians
 	Δφ := φ2 - φ1
 	Δλ := λ2 - λ1
-	a := math.Sin(Δφ/2)*math.Sin(Δφ/2) + math.Cos(φ1)*math.Cos(φ2)*math.Sin(Δλ/2)*math.Sin(Δλ/2)
+	a := math.Sin(Δφ/2)*math.Sin(Δφ/2) +
+		math.Cos(φ1)*math.Cos(φ2)*math.Sin(Δλ/2)*math.Sin(Δλ/2)
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 	return earthRadius * c
 }
 
-// DestinationPoint return the destination from a point based on a distance and bearing.
-func DestinationPoint(lat, lon, meters, bearingDegrees float64) (destLat, destLon float64) {
+// DestinationPoint return the destination from a point based on a
+// distance and bearing.
+func DestinationPoint(lat, lon, meters, bearingDegrees float64) (
+	destLat, destLon float64,
+) {
 	// see http://williams.best.vwh.net/avform.htm#LL
 	δ := meters / earthRadius // angular distance in radians
 	θ := bearingDegrees * radians
 	φ1 := lat * radians
 	λ1 := lon * radians
-	φ2 := math.Asin(math.Sin(φ1)*math.Cos(δ) + math.Cos(φ1)*math.Sin(δ)*math.Cos(θ))
-	λ2 := λ1 + math.Atan2(math.Sin(θ)*math.Sin(δ)*math.Cos(φ1), math.Cos(δ)-math.Sin(φ1)*math.Sin(φ2))
+	φ2 := math.Asin(math.Sin(φ1)*math.Cos(δ) +
+		math.Cos(φ1)*math.Sin(δ)*math.Cos(θ))
+	λ2 := λ1 + math.Atan2(math.Sin(θ)*math.Sin(δ)*math.Cos(φ1),
+		math.Cos(δ)-math.Sin(φ1)*math.Sin(φ2))
 	λ2 = math.Mod(λ2+3*math.Pi, 2*math.Pi) - math.Pi // normalise to -180..+180°
 	return φ2 * degrees, λ2 * degrees
+}
+
+// BearingTo returns the (initial) bearing from point 'A' to point 'B'.
+func BearingTo(latA, lonA, latB, lonB float64) float64 {
+	// tanθ = sinΔλ⋅cosφ2 / cosφ1⋅sinφ2 − sinφ1⋅cosφ2⋅cosΔλ
+	// see mathforum.org/library/drmath/view/55417.html for derivation
+
+	φ1 := latA * radians
+	φ2 := latB * radians
+	Δλ := (lonB - lonA) * radians
+	y := math.Sin(Δλ) * math.Cos(φ2)
+	x := math.Cos(φ1)*math.Sin(φ2) - math.Sin(φ1)*math.Cos(φ2)*math.Cos(Δλ)
+	θ := math.Atan2(y, x)
+
+	return math.Mod(θ*degrees+360, 360)
 }
