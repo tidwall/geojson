4082cc5b21df25bc4783c981d753406212065297
tidwall <joshbaker77@gmail.com>
2018-09-23 06:28:37 -0700

    Fix issue 360

--- rect_test.go
+++ rect_test.go
@@ -163,5 +163,4 @@ func TestRectAux(t *testing.T) {
 	expect(t, !R(10, 10, 20, 20).primativeContains(nil))
 	expect(t, !R(10, 10, 20, 20).primativeIntersects(nil))
 	expect(t, R(10, 10, 20, 20).Within(R(-100, -100, 1000, 1000)))
-
 }
